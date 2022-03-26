47e7624b041dee340a8028ac8cdff8abeb488340
dalamsepuluh <89678494+dalamsepuluh@users.noreply.github.com>
2022-03-27 01:43:45 +0800

    Update 5220600034_Anak Agung Inten Pramitriyani_Thread.cpp

--- 5220600034_Anak Agung Inten Pramitriyani_Thread.cpp
+++ 5220600034_Anak Agung Inten Pramitriyani_Thread.cpp
@@ -24,5 +24,20 @@ void oddThread () {
 		unique_lock <mutex> manageObj (Mutex);
 		// unique_lock sebuah objek yang mengelola object atau data dari mutex
 		
+		kondisi.wait (manageObj, []() {return (angka % 2 == 1);});
+		/* ketika variabel kondisi memeriksa penuh atau tidaknya, maka akan ada jeda/menunggu
+		untuk manageObject mengelola data, jika sudah maka akan return hasil loopingan dari inputan variabel
+		angka yaitu 1 mod 2 sama dengan 1 */
+		this_thread::sleep_for(chrono::seconds(1));
+		//untuk memberikan efek delay 1 detik
+		cout << "Odd Thread: " << angka << endl;
+		//untuk outputan Odd Thread sesuai angka yang telah dilooping
+		angka++;
+		//terus bertambah apabila outputan nilai sesuai dengan aturan looping
+		manageObj.unlock ();
+		//pengelolaan data akan terbuka, siap menjadi output
+		kondisi.notify_all();
+		/* variabel yang mengecek penuh atau tidak, juga dipakai untuk melancarkan output dari 
+		mengecek yang telah ter-unlock sehingga seluruh output dapat tertampilkan*/
 	}
 }
